99aac26c60c5d677aada08b88c7800c3dbb72267
Taylor Cooke <tcooke@relias.com>
2020-12-29 09:38:31 -0500

    initial editor showing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import 'core-js'
 
 Vue.config.productionTip = false
 
